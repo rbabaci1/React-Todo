04aa6a095b2d72241f3b7769a1b440fee385df35
rabah babaci <4rabah@gmail.com>
2020-03-09 19:01:27 -0700

    import TodoList to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import TodoList from './components/TodoList';
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
