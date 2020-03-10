d63331ce59653bd8014ba7125035fcc055f9850e
rabah babaci <4rabah@gmail.com>
2020-03-09 20:50:51 -0700

    add a clear completed button to the form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -5,6 +5,7 @@ const TodoForm = () => {
     <form>
       <input type='text' placeholder='...todo' />
       <button>Add Todo</button>
+      <button>Clear Completed</button>
     </form>
   );
 };
