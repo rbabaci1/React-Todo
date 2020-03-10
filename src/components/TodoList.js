7b60781203bab3e4cea902e6943e6d06ad895f6e
rabah babaci <4rabah@gmail.com>
2020-03-09 19:50:12 -0700

    define the Todo component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,8 +2,7 @@ import React from 'react';
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
 const TodoList = ({ todos }) => {
-  console.log(todos);
-  return <h1>todo list</h1>;
+  return null;
 };
 
 export default TodoList;
