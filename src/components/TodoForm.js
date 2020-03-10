f214d8920ad2fbe4168bdbed7af3c526846a2b54
rabah babaci <4rabah@gmail.com>
2020-03-09 20:01:53 -0700

    add an input and a button field to the TodoForm

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
 const TodoForm = () => {
-  return <p>Test</p>;
+  return (
+    <div className='todo-form'>
+      <input type='text' placeholder='...todo' />
+      <button>Add Todo</button>
+    </div>
+  );
 };
+
+export default TodoForm;
